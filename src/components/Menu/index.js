ee27832ce165fb79fc4504ce77bddd2f7fa5521d
Valker-Vinicius <valkerm2v@gmail.com>
2020-07-29 17:16:00 -0300

    New logo and border line color for Menu and Footer

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import logo from "../../assets/imgs/logo.png"
+import logo from "../../assets/imgs/logo1.png"
 import "./Menu.css"
 import Button from "../Button"
 
