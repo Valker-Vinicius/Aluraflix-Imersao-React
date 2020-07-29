a51b1e873e1889a16943d1d5bbed4d97e2abf68c
Valker-Vinicius <valkerm2v@gmail.com>
2020-07-29 14:35:43 -0300

    Form upgraded

--- src/pages/register/category/index.js
+++ src/pages/register/category/index.js
@@ -3,41 +3,92 @@ import { Link } from "react-router-dom"
 import PageDefault from "../../../components/PageDefault"
 
 function RegisterCategory() {
-  const [categories, setCategories] = useState(["Test"]) 
-  const [categoryName, setCategoryName] = useState("Movies")
+  const [categories, setCategories] = useState(["Default"]) 
   
+  const defaultValues = {
+    name: "",
+    description: "",
+    color: "#ffffff",
+  }
+  const [values, setValues] = useState(defaultValues)
+
+  function setValue(key, value) {
+    setValues ({
+      ...values,
+      [key]: value,
+    })
+  }
+
+  function handleChange(eventInfo) {
+    setValue(
+      eventInfo.target.getAttribute("name"), 
+      eventInfo.target.value
+    )
+  }
+
   return (
     <PageDefault>
-      <h1>Register category: {categoryName}</h1>
+      <h1>Register category: 
+        <span style= {{ color: values.color }}>
+          {" " + values.name}
+        </span>
+      </h1>
 
       <form onSubmit= {function handleSubmit(eventInfo) {
           eventInfo.preventDefault()
           setCategories([
             ...categories,
-            categoryName
+            values
           ])
         }}
       >
-        <label>
-          Category Name:
-          <input 
-          type= "text" 
-          placeholder= {categoryName} 
-          onChange= {function newValueCapture(eventInfo) {
-            setCategoryName(eventInfo.target.value)
-          }}
-        />
-        </label>
-        <button>
-          Register
-        </button>
+        <div>
+          <label>
+            Category Name:
+            <input
+            
+            type= "text"
+            placeholder= {values.name} 
+            name= "name"
+            onChange= {handleChange}
+          />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Description:
+            <textarea 
+            type= "text" 
+            placeholder= {values.description} 
+            name= "description"
+            onChange= {handleChange}
+          />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Color:
+            <input 
+            type= "color" 
+            placeholder= {values.color} 
+            name= "color"
+            onChange= {handleChange}
+          />
+          </label>
+
+          <button>
+            Register
+          </button>
+        </div>
       </form>
       
       <ul>
           {categories.map((category, index) => {
             return (
               <li key= {`${category}${index}`}>
-                {category}
+                {category.name}
               </li>
             )
           })}
